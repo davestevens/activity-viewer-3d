c7afe2e808083ced8d49cea2344d2a80975e9b31
Dave Stevens <d4v3.uk@googlemail.com>
2020-10-04 20:18:13 +0100

    Persists activity response in localstorage.

--- src/services/getActivity.ts
+++ src/services/getActivity.ts
@@ -19,8 +19,18 @@ const URL =
   "https://www.strava.com/api/v3/activities/{id}/streams?keys=latlng,velocity_smooth,altitude,heartrate&key_by_type=true";
 
 export const getActivity = async (id: string): Promise<IActivity> => {
+  const storedActivityKey = `ACTIVITY_${id}`;
+  const storedActivityData = JSON.parse(
+    localStorage.getItem(storedActivityKey)
+  );
+
+  if (storedActivityData) {
+    return storedActivityData;
+  }
   const url = URL.replace("{id}", id);
   const activity = await getRequest<IActivity>(url);
+
+  localStorage.setItem(storedActivityKey, JSON.stringify(activity));
 
   return activity;
 };
