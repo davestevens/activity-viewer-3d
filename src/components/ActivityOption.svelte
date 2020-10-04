dd5e0cf9e4b3d55d1fcb77e855da6881349d3761
Dave Stevens <d4v3.uk@googlemail.com>
2020-10-04 19:47:39 +0100

    Disables activities without latlng/heartrate data.

--- src/components/ActivityOption.svelte
+++ src/components/ActivityOption.svelte
@@ -2,8 +2,20 @@
   export let onSelect: (id: string) => void;
   import type { IActivity } from "../services/getActivities";
   export let activity: IActivity;
+
+  const isSelectable = activity.has_heartrate && !!activity.start_latlng;
 </script>
 
-<li class="nav-item" on:click={() => onSelect(activity.id)}>
+<style>
+  .isSelectable {
+    cursor: pointer;
+  }
+</style>
+
+<li
+  class="nav-item"
+  class:isSelectable
+  class:text-muted={!isSelectable}
+  on:click={() => isSelectable && onSelect(activity.id)}>
   <span class="navbar-text"> {activity.name} : {activity.start_date} </span>
 </li>
--- src/services/getActivities.ts
+++ src/services/getActivities.ts
@@ -4,7 +4,9 @@ export interface IActivity {
   name: string;
   type: string;
   id: string;
-  start_date: Date;
+  has_heartrate: boolean;
+  start_date: string;
+  start_latlng: number[];
 }
 
 const URL = "https://www.strava.com/api/v3/athlete/activities";
@@ -12,10 +14,5 @@ const URL = "https://www.strava.com/api/v3/athlete/activities";
 export const getActivities = async (): Promise<IActivity[]> => {
   const activities = await getRequest<IActivity[]>(URL);
 
-  return activities.map((activity) => ({
-    name: activity.name,
-    type: activity.type,
-    id: activity.id,
-    start_date: new Date(activity.start_date),
-  }));
+  return activities;
 };
