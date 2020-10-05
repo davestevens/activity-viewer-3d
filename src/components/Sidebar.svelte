01a30a4146d444b391565cbffca60271e421cf7b
Dave Stevens <d4v3.uk@googlemail.com>
2020-10-05 21:24:35 +0100

    Includes collapsable bottom sidebar.

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -2,6 +2,12 @@
   import SidebarHeader from "../components/SidebarHeader.svelte";
   import ActivityList from "../components/ActivityList.svelte";
   import ZonesData from "../components/ZonesData.svelte";
+
+  let collapsed: boolean = true;
+
+  const toggleBottom = () => {
+    collapsed = !collapsed;
+  };
 </script>
 
 <style>
@@ -9,21 +15,48 @@
     margin-top: 48px;
     width: 320px;
     height: calc(100vh - 48px);
+    display: flex;
+    flex-direction: column;
+    border-right: 1px solid #00000020;
   }
 
-  .sidebar-sticky {
+  .sidebar-top {
     overflow: auto;
-    height: 100%;
+    flex-shrink: 1;
+  }
+
+  .sidebar-bottom__button {
+    cursor: pointer;
+  }
+
+  .sidebar-bottom {
+    overflow: auto;
+    flex-shrink: 0;
+  }
+
+  .sidebar-bottom__collapsable.collapse {
+    height: 0;
   }
 </style>
 
 <nav class="sidebar">
-  <div class="sidebar-sticky">
+  <div class="sidebar-top">
     <SidebarHeader title="Activities" />
     <ActivityList />
+  </div>
 
-    <SidebarHeader title="Zones Data" />
-    <!-- TODO: expandable -->
-    <ZonesData />
+  <div class="sidebar-bottom">
+    <div
+      class="sidebar-bottom__button list-group-item d-flex justify-content-between align-items-center"
+      on:click={toggleBottom}>
+      Zones Data
+      <i
+        class="fas"
+        class:fa-chevron-up={collapsed}
+        class:fa-chevron-down={!collapsed} />
+    </div>
+    <div class="sidebar-bottom__collapsable" class:collapse={collapsed}>
+      <ZonesData />
+    </div>
   </div>
 </nav>
