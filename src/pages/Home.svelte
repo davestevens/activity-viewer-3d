804eddc87d81713c4a88be8a4f95a545d900c4f1
Dave Stevens <d4v3.uk@googlemail.com>
2020-10-05 21:32:31 +0100

    Manage errors during login.

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
   import { auth } from "../stores/auth";
 
+  let error: string = "";
+
   const handleAuth = (): void => {
     const popup = window.open("/popup.html", "_blank", "width=700,height=600");
     window.onmessage = (message: any): void => {
       if (message.data.type === "AUTH") {
-        auth.setAuth(message.data.data);
+        if (message.data.error) {
+          error = message.data.data;
+        } else {
+          auth.setAuth(message.data.data);
+        }
         popup.close();
       }
     };
@@ -16,7 +22,7 @@
 </style>
 
 <div class="row row h-100 justify-content-center align-items-center">
-  <main class="col-4">
+  <main class="col-4 text-center">
     <h1 class="display-3">Activity Viewer 3D</h1>
     <p class="lead">
       View your Strava activities in 3D space with heartrate data visualization.
@@ -25,5 +31,11 @@
       type="button"
       class="btn btn-primary btn-lg btn-block mb-2"
       on:click={handleAuth}>Login</button>
+    {#if error}
+      <div class="alert alert-danger" role="alert">
+        Something went wrong -
+        {error}
+      </div>
+    {/if}
   </main>
 </div>
