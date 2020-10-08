4d02edb978932707bc0c4c06ee43cd8b96da59ae
Dave Stevens <d4v3.uk@googlemail.com>
2020-10-08 20:24:02 +0100

    Position and rotate segments.

--- src/route/index.ts
+++ src/route/index.ts
@@ -126,22 +126,19 @@ export const renderRoute = (data: IData): void => {
 
   const SEGMENT_WIDTH = 1;
 
-  // TODO: figure out y axis rotation
-  // need to move pivot point
-  const drawSegment = (x1, y1, z1, x2, y2, z2): THREE.Group => {
+  const drawSegment = (length, z1, z2, color): THREE.Group => {
     const group = new THREE.Group();
 
-    const sectionLength = x2 - x1;
     const side = new THREE.Shape()
       .moveTo(0, 0)
-      .lineTo(sectionLength, 0)
-      .lineTo(sectionLength, z2)
+      .lineTo(length, 0)
+      .lineTo(length, z2)
       .lineTo(0, z1)
       .lineTo(0, 0);
 
     const pointHeightDiff = Math.abs(z2 - z1);
     const topLength = Math.sqrt(
-      Math.pow(sectionLength, 2) + Math.pow(pointHeightDiff, 2)
+      Math.pow(length, 2) + Math.pow(pointHeightDiff, 2)
     );
 
     const top = new THREE.Shape()
@@ -150,12 +147,12 @@ export const renderRoute = (data: IData): void => {
       .lineTo(topLength, SEGMENT_WIDTH)
       .lineTo(0, SEGMENT_WIDTH)
       .lineTo(0, 0);
-    const topAngle = Math.atan(pointHeightDiff / sectionLength);
+    const topAngle = Math.atan(pointHeightDiff / length);
 
     const sideGeometry = new THREE.ShapeBufferGeometry(side);
     const sideMesh = new THREE.Mesh(
       sideGeometry,
-      new THREE.MeshPhongMaterial({ color: 0xff0000, side: THREE.DoubleSide })
+      new THREE.MeshPhongMaterial({ color, side: THREE.DoubleSide })
     );
     const sideBMesh = sideMesh.clone();
     sideMesh.position.set(0, 0, 0);
@@ -169,19 +166,42 @@ export const renderRoute = (data: IData): void => {
     const topGeometry = new THREE.ShapeBufferGeometry(top);
     const topMesh = new THREE.Mesh(
       topGeometry,
-      new THREE.MeshPhongMaterial({ color: 0xff0000, side: THREE.DoubleSide })
+      new THREE.MeshPhongMaterial({ color, side: THREE.DoubleSide })
     );
     topMesh.position.set(0, 0, z1);
     topMesh.rotation.set(0, (z1 > z2 ? 1 : -1) * topAngle, 0);
     group.add(topMesh);
 
-    group.position.set(-sectionLength, -SEGMENT_WIDTH / 2, 0);
+    group.position.set(-length, -SEGMENT_WIDTH / 2, 0);
 
     return group;
   };
 
-  const segment = drawSegment(0, 0, 5, 2, 0, 6);
-  scene.add(segment);
+  const points = [
+    { x: 0, y: 0, z: 5, color: 0xff0000 },
+    { x: 1, y: 0, z: 6, color: 0x00ff00 },
+    { x: 2, y: 0, z: 6, color: 0x0000ff },
+    { x: 4, y: 0, z: 5, color: 0xff0000 },
+    { x: 4, y: 5, z: 5, color: 0x00ff00 },
+    { x: 6, y: 5, z: 3, color: 0x0000ff },
+  ];
+
+  for (let i = 1; i < points.length; ++i) {
+    const pointA = points[i - 1];
+    const pointB = points[i];
+    const x = pointB.x - pointA.x;
+    const y = pointB.y - pointA.y;
+    const segmentLength = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    const color = new THREE.Color(pointB.color);
+    const segment = drawSegment(segmentLength, pointA.z, pointB.z, color);
+
+    const group = new THREE.Group();
+    group.position.set(pointB.x, pointB.y, 0);
+    group.rotation.set(0, 0, Math.atan(y / x));
+    group.add(segment);
+
+    scene.add(group);
+  }
 
   var gridHelper = new THREE.GridHelper(100, 21);
   gridHelper.rotateX(Math.PI / 2);
